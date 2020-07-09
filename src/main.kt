b9111f39df01da416b95a838b70d7ee06e8b2bdd
keillani <keillani@github.com>
2020-07-09 19:43:45 -0300

    aula 03 conhecendo a orientacao a objetos

--- src/main.kt
+++ src/main.kt
@@ -2,43 +2,87 @@
 
 fun main() {
     println("Bem vindo ao ByteBank!")
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 200.0
 
-    //var mutavel
-    //val imutavel
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
 
-        val titular = "Keillani"
-        val numeroConta = 1000
-        var saldo = 100.00
-        saldo -= 1000.00
+    println (contaAlex.titular)
+    println (contaAlex.numero)
+    println (contaAlex.saldo)
+    println()
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+    println()
+    println("Depositando na conta do Alex")
+    deposita(contaAlex,valor=50.0)
+    println(contaAlex.saldo)
 
-        println("titular $titular")
-        println("numero conta $numeroConta")
-        println("saldo $saldo")
-        println()
+    println("Depositando na conta da Fran")
+    deposita(contaFran,valor=70.0)
+    println(contaFran.saldo)
+
+
 
 //    testaCondicoesIfWhen(saldo)
 //    testaFor()
 //    testaWhile()
-    loopBreakContinue()
+//    loopBreakContinue()
 
 }
 
-fun loopBreakContinue(){
-    loop@ for (i in 1..100){
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+fun deposita (conta: Conta, valor: Double){
+    conta.saldo+= valor
+}
+
+fun testaCopiaseReferencias(){
+    //aqui o numeroX não é alterado por ser um primitivo
+    val numeroX =10
+    var numeroY = numeroX
+    numeroY++
+    println(numeroX)
+    println(numeroY)
+
+    //aqui por se tratar de referências e apontar para mesma o que alterar em um altera no outro
+    val contaJoao = Conta()
+    contaJoao.titular= "João"
+
+    var contaMaria = contaJoao //correto: var contaMaria = Conta()
+    contaMaria.titular = "Maria"
+
+    println("titular conta joao: ${contaJoao.titular}")
+    println()
+    println("titurla conta maria: ${contaMaria.titular}")
+}
+
+fun loopBreakContinue() {
+    loop@ for (i in 1..100) {
         println("i $i")
-        for (j in 1..100){
+        for (j in 1..100) {
             println("j $j")
-            if(j == 5) break@loop
+            if (j == 5) break@loop
         }
     }
 }
 
-fun testaWhile(){
+fun testaWhile() {
 
     var i = 0
     while (i < 5) {
         val titular = "Keillani $i"
-        val numeroConta = 1000 +i
+        val numeroConta = 1000 + i
         var saldo = 100 + i
 
         println("titular $titular")
@@ -49,10 +93,10 @@ fun testaWhile(){
     }
 }
 
-fun testaFor(){
+fun testaFor() {
     for (i in 1..5) {
         val titular: String = "Keillani $i"
-        val numeroConta: Int = 1000 +i
+        val numeroConta: Int = 1000 + i
         var saldo = 100 + i
 
         println("titular $titular")
@@ -64,7 +108,7 @@ fun testaFor(){
     for (i in 5 downTo 1) {
 
         val titular = "Keillani $i"
-        val numeroConta = 1000 +i
+        val numeroConta = 1000 + i
         var saldo = 100 + i
 
         println("titular $titular")
@@ -76,7 +120,7 @@ fun testaFor(){
     for (i in 1..5 step 2) {
 
         val titular = "Keillani $i"
-        val numeroConta = 1000 +i
+        val numeroConta = 1000 + i
         var saldo = 100 + i
 
         println("titular $titular")
@@ -88,7 +132,7 @@ fun testaFor(){
     for (i in 1 until 5) {
 
         val titular = "Keillani $i"
-        val numeroConta = 1000 +i
+        val numeroConta = 1000 + i
         var saldo = 100 + i
 
         println("titular $titular")
