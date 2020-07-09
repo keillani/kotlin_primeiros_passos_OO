4ec37e51dc4a56dc32bc3d484ddcc79bf0992382
keillani <keillani@github.com>
2020-07-08 21:51:57 -0300

    aula 01 primeiros passos

---
+++ src/main.kt
@@ -0,0 +1,15 @@
+fun main(){
+    println("Bem vindo ao ByteBank!")
+
+    //var mutavel
+    //val imutavel
+
+    val titular = "Keillani"
+    val numeroConta = 12345
+    var saldo = 100 + 2.0
+    saldo += 200
+
+    println("titular $titular")
+    println("numero conta $numeroConta")
+    println("saldo $saldo")
+}
